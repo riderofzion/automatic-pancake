908a80a0b2e93733eaa47a4245540f9665e59e2c
stropee <stropee@eni-ecole.fr>
2024-01-08 15:01:33 +0100

    m02d04

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HelloWorld"
+include(":app")
+include(":mod2d03")
+include(":mod02d04")
